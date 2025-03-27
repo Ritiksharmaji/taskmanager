cba99ae35f2020c28d10b22f6154ef2f7888d188
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-03-27 21:34:21 +0530

    adding

--- frontend_taskmanager/src/services/api.js
+++ frontend_taskmanager/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; 
+// const API_URL = "http://localhost:5000/api"; 
+const API_URL = "https://taskmanager-phi-six.vercel.app/api"; 
+
+
 
 export const registerUser = async (userData) => {
   return axios.post(`${API_URL}/auth/register`, userData);
