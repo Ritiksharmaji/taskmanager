76ba598f80a058420c3f1e3d53109f5c41a14e7b
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-05-17 22:57:11 +0530

    adding websocket

--- backend_taskmanager/config/database.js
+++ backend_taskmanager/config/database.js
@@ -1,21 +1,21 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config(); 
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config(); 
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: "mysql", 
-  logging: false,
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: "mysql", 
+//   logging: false,
+// });
 
-async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log("Database connected successfully!");
-  } catch (error) {
-    console.error(" Unable to connect to database:", error);
-  }
-}
+// async function testConnection() {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Database connected successfully!");
+//   } catch (error) {
+//     console.error(" Unable to connect to database:", error);
+//   }
+// }
 
-testConnection();
+// testConnection();
 
-module.exports = sequelize;
+// module.exports = sequelize;
--- backend_taskmanager/models/User_relational.js
+++ backend_taskmanager/models/User_relational.js
@@ -1,31 +1,31 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+// const { DataTypes } = require("sequelize");
+// const sequelize = require("../config/database");
 
-const User = sequelize.define(
-  "User",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: false, 
-  }
-);
+// const User = sequelize.define(
+//   "User",
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true,
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     timestamps: false, 
+//   }
+// );
 
-module.exports = User;
+// module.exports = User;
